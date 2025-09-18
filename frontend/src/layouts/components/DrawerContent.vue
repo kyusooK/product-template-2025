7ad3c44a762cf31e4c2984b341b9160c1c111441
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2025-09-18 16:43:17 +0900

    commit

---
+++ frontend/src/layouts/components/DrawerContent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {
+  VerticalNavLink,
+  VerticalNavSectionTitle,
+} from '@layouts'
+</script>
+
+<template>
+    <ul>
+        <VerticalNavSectionTitle :item="{ heading: 'Product' }" />
+        <VerticalNavLink
+            :item="{
+                title: 'Product',
+                to: '/products',
+            }"
+        />
+    </ul>
+</template>
